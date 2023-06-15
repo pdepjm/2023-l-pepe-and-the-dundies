febf9a76ec53f96614636d0d4c96a915c4fccbbf
Diana López Alvas <dlopezalvas@gmail.com>
2023-06-15 13:10:00 -0300

    the dundies - resolución en clase

--- programa.pl
+++ programa.pl
@@ -57,7 +57,7 @@ consumoDeCafe(angela,1).
 consumoDeCafe(jim,2).
 consumoDeCafe(kevin,0).
 consumoDeCafe(oscar,1).
-consumoDeCafe(toby,3).
+consumoDeCafe(toby,30).
 consumoDeCafe(phyllis,4).
 consumoDeCafe(ryan,2).
 consumoDeCafe(kelly,3).
@@ -66,6 +66,7 @@ consumoDeCafe(stanley,4).
 consumoDeCafe(meredith,1).
 consumoDeCafe(erin,0).
 consumoDeCafe(darryl,0).
+consumoDeCafe(pam, 2).
 
 %importancia/2 -> Dundie, Importancia
 
@@ -82,3 +83,71 @@ importancia(crucigrama,15).
 importancia(peorVendedor,5).
 importancia(abejaMasOcupada,10).
 importancia(compromisoMasLargo,15).
+
+% Punto 1
+
+tomoMuchoCafe(Empleado):-
+    consumoDeCafe(Empleado, Cantidad),
+    Cantidad > 10.
+
+% Punto 2
+%ganoDundie(Empleado, Dundie)
+
+ganoDundie(michael, mejorJefeDelMundo).
+ganoDundie(dwight, sensei).
+ganoDundie(jim, jimothy).
+ganoDundie(jim, mejorPapa).
+ganoDundie(meredith, mejorMama).
+ganoDundie(angela, masPequenia).
+ganoDundie(pam, zapatosBlancos).
+ganoDundie(ryan, masAtractivoDeLaOficina).
+ganoDundie(kelly, mejorCoqueteo).
+ganoDundie(stanley, crucigrama).
+ganoDundie(oscar, peorVendedor).
+ganoDundie(phyllis, abejaMasOcupada).
+ganoDundie(meredith, compromisoMasLargo).
+
+% Punto 3
+%nuncaGanoDundie(Empleado)
+
+%empleado(Nombre, Antiguedad, CafeAlDia, FaltasDelMes) por ejemplo podría ser
+empleado(Empleado):-
+    consumoDeCafe(Empleado,_).
+
+nuncaGanoDundie(Empleado):-
+    empleado(Empleado),
+    not(ganoDundie(Empleado,_)).
+
+ganoMasDeUnaVez(Empleado):-
+    ganoDundie(Empleado, Dundie1),
+    ganoDundie(Empleado, Dundie2),
+    Dundie1 \= Dundie2.
+
+ganoUnaSolaVez(Empleado):-
+    ganoDundie(Empleado, _),
+    not(ganoMasDeUnaVez(Empleado)).
+
+% Punto 4:
+%puntaje(Empleado, Dundie, Puntaje)
+puntaje(Empleado, Dundie, Puntaje):-
+    ganoDundie(Empleado, Dundie),
+    importancia(Dundie, Importancia),
+    consumoDeCafe(Empleado, Consumo),
+    Puntaje is Importancia * Consumo. 
+
+% Punto 5: 
+% ¿Qué nombre tiene mi predicado? ganadorSupremo
+% ¿Qué aridad tiene? ¿Qué estoy relacionando? /1 -> Empleado
+% ¿Es un hecho, una regla? ¿Tiene muchas cláusulas? Necesito una regla
+
+% No existe alguien que tenga un puntaje mayor al empleado
+
+ganadorSupremo(Empleado):-
+    ganoDundie(Empleado, _),
+    not(tienePuntajeMayor(_, Empleado)).
+
+% Quiero saber si un empleado tiene mayor puntaje que otro
+tienePuntajeMayor(EmpleadoMayor, EmpleadoMenor):-
+    puntaje(EmpleadoMayor, _, PuntajeMayor),
+    puntaje(EmpleadoMenor, _, PuntajeMenor),
+    PuntajeMayor > PuntajeMenor.
